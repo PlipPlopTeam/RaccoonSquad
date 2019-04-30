064660da14dd5d64a77992d9af3d1db7a763602f
Louis Perdu <l.perdu@rubika-edu.com>
2019-04-30 08:50:54 +0200

    📚 Creating the Library class that holds assets references

---
+++ RacoonSquad/Assets/Library.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Library : MonoBehaviour
+{
+    public static Library instance;
+
+    [Header("Prefabs")]
+    public GameObject racoonPrefab;
+
+    void Awake()
+    {
+        instance = this;
+    }
+}
--- RacoonSquad/Assets/Scripts/GameManager.cs
+++ RacoonSquad/Assets/Scripts/GameManager.cs
@@ -7,7 +7,6 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
-    public GameObject racoonPrefab;
     public bool lobby = false;
     public int playerCount;
 
@@ -32,7 +31,7 @@ public class GameManager : MonoBehaviour
     {
         for(int i = 0; i < players.Length; i++)
         {
-            PlayerController pc = Instantiate(racoonPrefab).GetComponent<PlayerController>();
+            PlayerController pc = Instantiate(Library.instance.racoonPrefab).GetComponent<PlayerController>();
             pc.gameObject.name = "Racoon_" + players[i];
 
             pc.index = players[i];
@@ -43,7 +42,7 @@ public class GameManager : MonoBehaviour
     {
         for(int i = 0; i < playerCount; i++)
         {
-            PlayerController pc = Instantiate(racoonPrefab).GetComponent<PlayerController>();
+            PlayerController pc = Instantiate(Library.instance.racoonPrefab).GetComponent<PlayerController>();
             pc.gameObject.name = "Racoon_" + i;
             pc.index = (PlayerIndex)i;
         }   
