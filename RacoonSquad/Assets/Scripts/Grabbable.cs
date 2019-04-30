a4f9b9c0fef0ab261d4323ea5b4e324c1679953f
rackover <rackover@live.fr>
2019-04-30 09:39:08 +0200

    Added sample props

--- RacoonSquad/Assets/Scripts/Grabbable.cs
+++ RacoonSquad/Assets/Scripts/Grabbable.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class Grabbable : MonoBehaviour
 {
-    
+    Rigidbody rb;
+    public new Collider collider;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
+    }
+
+    public void GetHeldBy(Transform _transform, Vector3 offset=new Vector3())
+    {
+        transform.parent = _transform;
+        transform.localPosition = offset;
+        rb.isKinematic = true;
+        gameObject.layer = LayerMask.NameToLayer("NoPlayerCollision");
+    }
+
+    public void GetDropped()
+    {
+        transform.parent = null;
+        rb.isKinematic = false;
+        gameObject.layer = 0; // Default layer
+    }
 }
