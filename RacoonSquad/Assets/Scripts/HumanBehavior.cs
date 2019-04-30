f14552bbbfec11d62ef1d26fa910669628ee5ec7
Louis Perdu <l.perdu@rubika-edu.com>
2019-04-30 16:02:01 +0200

    Updating Human Behavior

--- RacoonSquad/Assets/Scripts/HumanBehavior.cs
+++ RacoonSquad/Assets/Scripts/HumanBehavior.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public enum HumanState{ Walking, Chasing }
+public enum HumanState{ Walking, Thinking, Chasing, Collecting }
 
 public class HumanBehavior : MonoBehaviour
 {
@@ -27,6 +27,7 @@ public class HumanBehavior : MonoBehaviour
     NavMeshAgent agent;
     Sight sight;
     PlayerController seenPlayer;
+    Grabbable seenItem;
 
     void Awake()
     {
@@ -41,19 +42,15 @@ public class HumanBehavior : MonoBehaviour
             case HumanState.Walking:
                 targetSpeed = walkSpeed;
                 break;
+            case HumanState.Thinking:
+                targetSpeed = 0;
+                break;
             case HumanState.Chasing:
                 targetSpeed = chaseSpeed;
                 break;
         }
 
         state = newState;
-    }
-
-    void Mark()
-    {
-        if(mark != null) Destroy(mark);
-        mark = Instantiate(Library.instance.exclamationMarkPrefab, transform);
-        mark.transform.localPosition = new Vector3(0f, 5.25f, 0f);
     }
 
     void Start()
@@ -67,17 +64,94 @@ public class HumanBehavior : MonoBehaviour
         // Lerp agent speed for a more organic effect
         agent.speed = Mathf.Lerp(agent.speed, targetSpeed, velocityLerpSpeed * Time.deltaTime);
 
+        // Different update depending on the current state
+        StateUpdate();
+
+        // Check if the human has reach his destination
+        if(Vector3.Distance(transform.position, agent.destination) < navTreshold) StateDestinationReached();
+    }
+
+    void StateUpdate()
+    {
         switch(state)
         {
             case HumanState.Walking:
-                // Check if the Human as reach his destination
-                if(Vector3.Distance(transform.position, agent.destination) < navTreshold) MoveTo(GetNextWaypoint());
-                // Scanning for Racoons
                 ScanRacoons();
                 break;
+
             case HumanState.Chasing:
+                agent.destination = seenPlayer.transform.position;
+                if(seenPlayer.GetHeldObject() == null) ChangeState(HumanState.Collecting);
+                break;
+
+            case HumanState.Collecting:
+                agent.destination = seenItem.transform.position;
                 break;
         }
+    }
+    void StateDestinationReached()
+    {
+        switch(state)
+        {
+            case HumanState.Walking:
+                MoveTo(GetNextWaypoint());
+                break;
+
+            case HumanState.Chasing:
+                //seenPlayer.DropHeldObject();
+                MoveTo(GetNextWaypoint());
+                break;
+
+            case HumanState.Collecting:
+                Destroy(seenItem);
+                MoveTo(GetNextWaypoint());
+                break;
+        }
+    }
+
+    IEnumerator Thinking(Vector3 position)
+    {
+        // Look at the intresting thing
+        Vector3 direction = seenPlayer.transform.position - transform.position;
+        direction = new Vector3(direction.x, transform.position.y, direction.z);
+        transform.forward = direction;
+
+        // Stops agent movement
+        //agent.destination = transform.position;
+
+        // Put a mark on his head
+        Mark();
+
+        ChangeState(HumanState.Thinking);
+        // Trigger Animation
+        
+        yield return new WaitForSeconds(1f);
+
+        Unmark();
+
+        seenItem = seenPlayer.GetHeldObject();
+        if(seenItem != null)
+        {
+            ChangeState(HumanState.Chasing);
+        }
+        else
+        {
+            ChangeState(HumanState.Walking);
+            if(paths.Length > 0) MoveTo(0);
+        }
+        // Return to normal state or chasing
+    }
+
+    // EXCLAMATION MARK ABOVE HEAD
+    void Mark()
+    {
+        if(mark != null) Destroy(mark);
+        mark = Instantiate(Library.instance.exclamationMarkPrefab, transform);
+        mark.transform.localPosition = new Vector3(0f, 5.25f, 0f);
+    }
+    void Unmark()
+    {
+        if(mark != null) Destroy(mark);
     }
 
     void ScanRacoons()
@@ -88,11 +162,16 @@ public class HumanBehavior : MonoBehaviour
             PlayerController pc = go.GetComponent<PlayerController>();
             if(pc != null)
             {
-                seenPlayer = pc;
-                agent.destination = pc.transform.position;
-                ChangeState(HumanState.Chasing);
+                SpotRaccoon(pc);
             }
         }
+    }
+
+    void SpotRaccoon(PlayerController pc)
+    {
+        seenPlayer = pc;
+        
+        StartCoroutine(Thinking(seenPlayer.transform.position));
     }
 
     int GetNextWaypoint()
