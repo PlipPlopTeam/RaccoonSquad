130cf286baad48905a6bf2801c33ef014608680b
rackover <rackover@live.fr>
2019-04-30 11:27:42 +0200

    Added grabbable value and drop zone

---
+++ RacoonSquad/Assets/Scripts/GoalZone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        var prop = other.GetComponent<Grabbable>();
+        if (prop && !prop.IsHeld()) {
+            Destroy(prop.gameObject);
+            GameManager.instance.level.currentScore += prop.racoonValue;
+        }
+    }
+}
