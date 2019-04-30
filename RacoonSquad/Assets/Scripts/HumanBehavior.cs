c8c2e368bfee8cd4f97201b8620e5d9dac3b9dfc
Louis Perdu <l.perdu@rubika-edu.com>
2019-04-30 12:12:16 +0200

    👀 Adding Sight system and a quick implementation in the Human Behavior

--- RacoonSquad/Assets/Scripts/HumanBehavior.cs
+++ RacoonSquad/Assets/Scripts/HumanBehavior.cs
@@ -1,18 +1,103 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
+enum HumanState{ Calm, Aware, Angry }
 
 public class HumanBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Settings")]
+    public float walkSpeed;
+    public float chaseSpeed;
+    public float velocityLerpSpeed = 1f;
+    public float navTreshold = 1f;
+
+    [Header("Path")]
+    public Transform[] paths;
+    int currentWaypoint;
+
+    float targetSpeed;
+
+    // Referencies
+    NavMeshAgent agent;
+    Sight sight;
+    PlayerController seenPlayer;
+
+    void Awake()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        sight = GetComponent<Sight>();
     }
 
-    // Update is called once per frame
+    void ChangeState(HumanState state)
+    {
+        switch(state)
+        {
+            case HumanState.Calm:
+                targetSpeed = walkSpeed;
+                break;
+            case HumanState.Aware:
+                targetSpeed = walkSpeed;
+                break;
+            case HumanState.Angry:
+                targetSpeed = chaseSpeed;
+                break;
+        }
+    }
+
+    void Start()
+    {
+        ChangeState(HumanState.Calm);
+
+        if(paths.Length > 0)
+        {
+            MoveTo(0);
+        }
+    }
+    
     void Update()
     {
-        
+        // Lerp agent speed for a more organic effect
+        agent.speed = Mathf.Lerp(agent.speed, targetSpeed, velocityLerpSpeed * Time.deltaTime);
+
+        // Check if the Human as reach his destination
+        if(Vector3.Distance(transform.position, agent.destination) < navTreshold)
+        {
+            MoveTo(GetNextWaypoint());
+        }
+
+        ScanRacoons();
+    }
+
+    void ScanRacoons()
+    {
+        GameObject[] seens = sight.Scan();
+
+        foreach(GameObject go in seens)
+        {
+            PlayerController pc = go.GetComponent<PlayerController>();
+            if(pc != null)
+            {
+                seenPlayer = pc;
+                agent.destination = pc.transform.position;
+                ChangeState(HumanState.Angry);
+            }
+        }
+    }
+
+    int GetNextWaypoint()
+    {
+        int waypoint = currentWaypoint + 1;
+        if(waypoint >= paths.Length) waypoint = 0;
+        return waypoint;
+    }
+
+    void MoveTo(int waypointIndex)
+    {
+        if(paths.Length == 0) return;
+
+        agent.destination = paths[waypointIndex].position;
+        currentWaypoint = waypointIndex;
     }
 }
---
+++ RacoonSquad/Assets/Sight.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sight : MonoBehaviour
+{
+    public float headHeight = 0f;
+    public float fieldOfViewAngle = 110f;
+    public float range = 5f;
+
+    public GameObject[] Scan()
+    {
+        List<GameObject> objects = new List<GameObject>();
+
+        Vector3 headPosition = transform.position + new Vector3(0f, headHeight, 0f);
+
+        RaycastHit[] hits;
+        hits = Physics.SphereCastAll(headPosition, range, transform.forward, Mathf.Infinity);
+
+        for(int i = 0; i < hits.Length; i++)
+        {
+            Vector3 direction = hits[i].transform.position - headPosition;
+            float angle = Vector3.Angle(direction, transform.forward);
+
+            if(angle < fieldOfViewAngle * 0.5f)
+            {
+                objects.Add(hits[i].transform.gameObject);
+            }
+        }
+
+        return objects.ToArray();
+    }
+}
