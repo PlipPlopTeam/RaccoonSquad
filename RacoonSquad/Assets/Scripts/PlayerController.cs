aaa0b9e64d059f8017ebbe0c87bd5e767ec0de5e
rackover <rackover@live.fr>
2019-04-30 09:39:45 +0200

    Added grab code

--- RacoonSquad/Assets/Scripts/PlayerController.cs
+++ RacoonSquad/Assets/Scripts/PlayerController.cs
@@ -5,10 +5,10 @@ using XInputDotNetPure;
 
 public class PlayerController : MonoBehaviour
 {
-
-    // Referencies
     public PlayerIndex index;
-    Rigidbody rb;
+
+    [Header("Inputs")]
+    public float grabTriggerThreshold = 0.3f;
 
     [Header("Locomotion")]
     public float speedForce = 25f;
@@ -18,25 +18,26 @@ public class PlayerController : MonoBehaviour
     public List<Grabbable> objectsAtRange = new List<Grabbable>();
 
     CollisionEventTransmitter grabCollisions;
+    Grabbable heldObject;
     Vector3 targetOrientation;
+    Rigidbody rb;
+    new CapsuleCollider collider;
 
 
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        collider = GetComponent<CapsuleCollider>();
 
-        /*
         // Check which objects are currently grabbable
         grabCollisions = GetComponentInChildren<CollisionEventTransmitter>();
         grabCollisions.onTriggerEnter += (Collider x) => { var grab = x.GetComponent<Grabbable>(); if (grab) objectsAtRange.Add(grab); };
         grabCollisions.onTriggerExit += (Collider x) => { var grab = x.GetComponent<Grabbable>(); if (grab) objectsAtRange.Remove(grab); };
-        */
+        
     }
 
     void Update()
     {
-        // Debug
-        //if(controller.gamepad.GetButtonDown("A")) Debug.Log(gameObject.name);
         var state = GamePad.GetState(index);
         CheckInputs(state);
     }
@@ -65,11 +66,53 @@ public class PlayerController : MonoBehaviour
 
     void CheckGrabInputs(GamePadState state)
     {
-        
+        if (state.Triggers.Right > grabTriggerThreshold) {
+            // Trigger is pressed
+            if (IsHolding()) {
+                // Nothing - keep holding
+            }
+            else if (IsAnythingAtRange()) {
+                // Grab the first object you saw
+                GrabProp(objectsAtRange[0]);
+            }
+        }
+        else {
+            if (IsHolding()) {
+                DropHeldObject();
+            }
+        }
     }
 
     void GrabProp(Grabbable prop)
     {
+        float headHeight = 
+            collider.bounds.extents.y / 2f 
+            + prop.collider.bounds.extents.y / 2f 
+            - prop.collider.bounds.center.y 
+            + collider.bounds.center.y;
 
+        prop.GetHeldBy(transform, new Vector3(0f, headHeight, 0f));
+        heldObject = prop;
+    }
+
+    void DropHeldObject()
+    {
+        heldObject.GetDropped();
+        heldObject = null;
+    }
+
+    void ThrowHeldObject()
+    {
+
+    }
+
+    bool IsHolding()
+    {
+        return heldObject != null;
+    }
+
+    bool IsAnythingAtRange()
+    {
+        return objectsAtRange.Count > 0;
     }
 }
