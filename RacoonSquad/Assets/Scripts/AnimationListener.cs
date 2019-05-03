d127895f9f2e896c1ba69c269ba4fda1bee81023
Dorian Beaugendre <TahitiP4ncake@users.noreply.github.com>
2019-05-03 21:30:54 +0200

    bruit de pas raccoon

---
+++ RacoonSquad/Assets/Scripts/AnimationListener.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationListener : MonoBehaviour
+{
+    public void Step()
+    {
+        SoundPlayer.Play("si_step",.3f,Random.Range(.9f,1.1f));
+    }
+}
